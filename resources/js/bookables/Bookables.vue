c4d54cb9409f26de91d1cac05a79e701e6472bd1
beingshaif <blazing.shaif@gmail.com>
2022-05-22 22:36:36 +0530

    data , created , computed components added

--- resources/js/bookables/Bookables.vue
+++ resources/js/bookables/Bookables.vue
@@ -1,15 +1,21 @@
 <template>
   <div>
-    <bookable-list-item
-      item-title="Cheap Villa"
-      item-content="A budget friendly villa"
-      v-bind:price="100000"
-    ></bookable-list-item>
-    <bookable-list-item
-      item-title="Costly Villa"
-      item-content="A rich friendly villa"
-      v-bind:price="1500000"
-    ></bookable-list-item>
+    Rows is: {{ rows }}
+    <div v-if="!loading">
+      <div class="card">
+        <div class="card-header">Data is Loading......</div>
+      </div>
+    </div>
+
+    <div v-else>
+      <bookable-list-item
+        :item-title="bookable.title"
+        :item-content="bookable.content"
+        :price="bookable.price"
+        v-for="(bookable, index) in bookables"
+        :key="index"
+      ></bookable-list-item>
+    </div>
   </div>
 </template>
 
@@ -22,11 +28,59 @@ export default {
   },
   data() {
     return {
-
-    }
+      bookables: null,
+      loading: false,
+      columns: 3,
+    };
+  },
+  computed: {
+    rows() {
+      return this.bookables == null
+        ? 0
+        : Math.ceil(this.bookables.length / this.columns);
+    },
   },
   created() {
-    console.log("Created");
+    setTimeout(() => {
+      this.loading = true;
+      this.bookables = [
+        {
+          title: "Cheap Villa..",
+          content: "A budget friendly villa",
+          price: 150000,
+        },
+        {
+          title: "Costly Villa",
+          content: "A rich friendly villa",
+          price: 2000000,
+        },
+        {
+          title: "Costly Villa",
+          content: "A rich friendly villa",
+          price: 2000000,
+        },
+        {
+          title: "Costly Villa",
+          content: "A rich friendly villa",
+          price: 2000000,
+        },
+        {
+          title: "Costly Villa",
+          content: "A rich friendly villa",
+          price: 2000000,
+        },
+        {
+          title: "Costly Villa",
+          content: "A rich friendly villa",
+          price: 2000000,
+        },
+        {
+          title: "Costly Villa",
+          content: "A rich friendly villa",
+          price: 2000000,
+        },
+      ];
+    }, 2000);
   },
 };
 </script>

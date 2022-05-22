ac84e6b7c2de788413a4f9b8be022db90b73c3a4
beingshaif <blazing.shaif@gmail.com>
2022-05-22 20:11:34 +0530

    Vue  Components created

---
+++ resources/js/bookables/BookableListItem.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-md-8">
+        <div class="card">
+          <div class="card-header">{{ itemTitle }}</div>
+
+          <div class="card-body">
+            {{ itemContent }}, <small>{{ price }}</small>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: { itemTitle: String, itemContent: String, price: Number },
+};
+</script>
---
+++ resources/js/bookables/Bookables.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <bookable-list-item
+      item-title="Cheap Villa"
+      item-content="A budget friendly villa"
+      v-bind:price="100000"
+    ></bookable-list-item>
+    <bookable-list-item
+      item-title="Costly Villa"
+      item-content="A rich friendly villa"
+      v-bind:price="1500000"
+    ></bookable-list-item>
+  </div>
+</template>
+
+<script>
+import BookableListItem from "./BookableListItem";
+
+export default {
+  components: {
+    BookableListItem,
+  },
+  data() {
+    return {
+
+    }
+  },
+  created() {
+    console.log("Created");
+  },
+};
+</script>

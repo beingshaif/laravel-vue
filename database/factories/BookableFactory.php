892d49a4e14b2045bed423fecea0809fa99312fa
beingshaif <blazing.shaif@gmail.com>
2022-05-23 21:44:18 +0530

    Bookable factory added

--- database/factories/BookableFactory.php
+++ database/factories/BookableFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use Illuminate\Support\Arr;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class BookableFactory extends Factory
@@ -13,8 +14,21 @@ class BookableFactory extends Factory
      */
     public function definition()
     {
+        $suffix = [
+            'Villa',
+            'House',
+            'Cottage',
+            'Luxury Villas',
+            'Cheap House',
+            'Rooms',
+            'Cheap Rooms',
+            'Luxury Rooms',
+            'Fancy Rooms',
+        ];
+
         return [
-            //
+            'title' => $this->faker->city(). ' '. Arr::random($suffix),
+            'description' => $this->faker->text()
         ];
     }
 }

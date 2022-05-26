cce82de183ef9223d3f92b196a342df85357a8c4
beingshaif <blazing.shaif@gmail.com>
2022-05-26 22:51:03 +0530

    New Promise created

--- resources/js/bookables/Bookables.vue
+++ resources/js/bookables/Bookables.vue
@@ -60,6 +60,18 @@ export default {
     }
   },
   created() {
+    const p = new Promise((resolve, reject) => {
+        console.log(resolve);
+        console.log(reject);
+        setTimeout(() => {
+            resolve("Hello");
+        }, 3000);
+    })
+    .then(result => "Hello Again" + result)
+    .then(result => console.log(`Success ${result}`))
+    .catch(result => console.log(`Error ${result}`))
+    ;
+    console.log(p);
     setTimeout(() => {
       this.loading = true;
       this.bookables = [

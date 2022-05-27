bdd3af2e4cc0fe2519968e2f8f66845ef12f9a07
beingshaif <blazing.shaif@gmail.com>
2022-05-27 21:30:28 +0530

    Data items changed

--- resources/js/bookables/BookableListItem.vue
+++ resources/js/bookables/BookableListItem.vue
@@ -2,13 +2,13 @@
   <div class="card">
     <div class="card-body">
         <h5 class="card-title">{{ itemTitle }}</h5>
-        <p>{{ itemContent }}, <small>{{ price }}</small></p>
+        <p>{{ itemContent }}</p>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  props: { itemTitle: String, itemContent: String, price: Number },
+  props: { itemTitle: String, itemContent: String },
 };
 </script>

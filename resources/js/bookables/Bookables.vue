23154a9c74dd3e7f7a036fecbbaedcbe0ae2c9fd
beingshaif <blazing.shaif@gmail.com>
2022-05-27 21:30:50 +0530

    getting data through api

--- resources/js/bookables/Bookables.vue
+++ resources/js/bookables/Bookables.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     Rows is: {{ rows }}
-    <div v-if="!loading">
+    <div v-if="loading">
       <div class="card mb-4">
         <div class="card-body">Data is Loading......</div>
       </div>
@@ -15,16 +15,14 @@
         >
           <bookable-list-item
             :item-title="bookable.title"
-            :item-content="bookable.content"
-            :price="bookable.price"
+            :item-content="bookable.description"
           ></bookable-list-item>
         </div>
         <div
           class="col"
           v-for="p in placeholdersInRow(row)"
           :key="'placeholder' + row + p"
-        >
-        </div>
+        ></div>
       </div>
     </div>
   </div>
@@ -56,52 +54,26 @@ export default {
       return this.bookables.slice((row - 1) * this.columns, row * this.columns);
     },
     placeholdersInRow(row) {
-        return this.columns - this.bookablesInRow(row).length;
-    }
+      return this.columns - this.bookablesInRow(row).length;
+    },
   },
   created() {
     const p = new Promise((resolve, reject) => {
-        console.log(resolve);
-        console.log(reject);
-        setTimeout(() => {
-            resolve("Hello");
-        }, 3000);
+      console.log(resolve);
+      console.log(reject);
+      setTimeout(() => {
+        resolve("Hello");
+      }, 3000);
     })
-    .then(result => "Hello Again" + result)
-    .then(result => console.log(`Success ${result}`))
-    .catch(result => console.log(`Error ${result}`))
-    ;
+      .then((result) => "Hello Again" + result)
+      .then((result) => console.log(`Success ${result}`))
+      .catch((result) => console.log(`Error ${result}`));
     console.log(p);
-    setTimeout(() => {
-      this.loading = true;
-      this.bookables = [
-        {
-          title: "Cheap Villa..",
-          content: "A budget friendly villa",
-          price: 150000,
-        },
-        {
-          title: "Costly Villa 1",
-          content: "A rich friendly villa",
-          price: 2000000,
-        },
-        {
-          title: "Costly Villa 2",
-          content: "A rich friendly villa",
-          price: 2000000,
-        },
-        {
-          title: "Costly Villa 3",
-          content: "A rich friendly villa",
-          price: 2000000,
-        },
-        {
-          title: "Costly Villa 4",
-          content: "A rich friendly villa",
-          price: 2000000,
-        },
-      ];
-    }, 2000);
+
+    const request = axios.get("/api/bookables").then(response => {
+      this.bookables = response.data;
+      this.loading = false;
+    });
   },
 };
 </script>
